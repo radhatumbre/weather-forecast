ec457b1d57d69593a0b408f908c7548fe87e6d3c
Chayan Barman <barmanchayan10@gmail.com>
2023-10-11 19:17:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButton from "./components/TopButton";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TempDetails from "./components/TempDetails";
+import Forecast from "./components/Forecast";
+import getWeatherData from "./services/WeatherService";
+import getFormattedWeatherData from "./services/WeatherService";
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
+  const [query, setQuery] = useState({ q: "kolkata" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+ 
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData(query, units).then((data) => {
+        setWeather(data);
+      });
+     
+    };
+    fetchWeather();
+     
+  }, [query, units]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-slate-900 max-h-full min-h-screen">
+      <div className={` mx-auto max-w-screen-md min-h-screen py-5 px-32 bg-gradient-to-r from-[rgb(15,23,42)] via-[rgb(88,28,135)] to-[rgb(15,23,42) rounded-lg`}>
+        <TopButton setQuery={setQuery}/>
+        <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
+        {weather && (
+          <div>
+            <TimeAndLocation weather={weather}/>
+            <TempDetails weather={weather}/>
+            <Forecast title="hourly forecast" items={weather.hourly} />
+            <Forecast title="daily forecast" items={weather.daily} />
+            <Footer/>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
